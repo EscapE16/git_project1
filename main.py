085f5c540f4d5848b580d194ed503452e4f686f2
СОШ 1 <karimislamov745@gmail.com>
2024-12-02 20:37:28 +0300

    разные кружки

--- main.py
+++ main.py
@@ -5,65 +5,22 @@ from random import randint
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import Qt, QPoint
 from PyQt6 import uic
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow
-
-
-
-UI = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>310</x>
-      <y>410</y>
-      <width>141</width>
-      <height>61</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>PushButton</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>26</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton
 
 
 class Example(QMainWindow):
     def __init__(self):
         super(Example, self).__init__()
-        f = io.StringIO(UI)
-        uic.loadUi(f, self)
-        self.circles = []
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 100, 600, 600)
+        self.pushButton = QPushButton('Рисовальщик кружочков', self)
+        self.pushButton.resize(150, 50)
+        self.pushButton.move(230, 500)
         self.pushButton.clicked.connect(self.run)
+
+        self.circles = []
 
     def run(self):
         diameter = randint(5, 100)
